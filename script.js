c09695ff344698ee1cfc13b9cf0d00e5b5c81503
Fra075 <francesco.duchetta075@gmail.com>
2025-02-24 03:10:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', () => {
   const animazioneBenvenuti = document.getElementById('animazione-benvenuti');
   const mainContent = document.getElementById('main-content');
-
+  mainContent.style.display = 'block';
+  mainContent.style.opacity = 1; // Mostra il contenuto con una transizione
   setTimeout(() => {
     animazioneBenvenuti.style.opacity = 0;
     setTimeout(() => {
